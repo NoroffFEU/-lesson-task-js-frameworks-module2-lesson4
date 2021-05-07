d7b76672e123c13d287ae71abb1db7a79b35dc02
Raymond Johannessen <rajohan1@gmail.com>
2021-05-07 13:19:21 +0200

    Update to use react-hook-form v7

--- src/App.js
+++ src/App.js
@@ -12,73 +12,84 @@ import { MINIMUM_NAME_CHARACTERS, PASSPORT_REGEX, SKILLS, DEFAULT_VALUES } from 
 import "./App.css";
 
 const schema = yup.object().shape({
-	name: yup.string().required("Please enter your name").min(MINIMUM_NAME_CHARACTERS, `Your name must at be at least ${MINIMUM_NAME_CHARACTERS} characters`),
-	email: yup.string().required("Please enter your email address").email("Please enter a valid email address"),
-	password: yup.string().required("Please enter your password").matches(PASSPORT_REGEX, "Your password is not valid"),
-	confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords must match"),
+    name: yup
+        .string()
+        .required("Please enter your name")
+        .min(MINIMUM_NAME_CHARACTERS, `Your name must at be at least ${MINIMUM_NAME_CHARACTERS} characters`),
+    email: yup.string().required("Please enter your email address").email("Please enter a valid email address"),
+    password: yup.string().required("Please enter your password").matches(PASSPORT_REGEX, "Your password is not valid"),
+    confirmPassword: yup.string().oneOf([yup.ref("password"), null], "Passwords must match")
 });
 
 function App() {
-	const [submitted, setSubmitted] = useState(false);
+    const [submitted, setSubmitted] = useState(false);
 
-	const { register, handleSubmit, control, reset, formState: { errors } } = useForm({
-		resolver: yupResolver(schema),
-	});
+    const {
+        register,
+        handleSubmit,
+        control,
+        reset,
+        formState: { errors }
+    } = useForm({
+        resolver: yupResolver(schema)
+    });
 
-	function onSubmit(data) {
-		console.log(data);
-		// you would normally do a POST or PUT request here
-		// set submitted to true so that the Alert displays
-		setSubmitted(true);
-		// we need to provide a default values object to clear the React Select value
-		// https://react-hook-form.com/api/#reset
-		reset(DEFAULT_VALUES);
-	}
+    function onSubmit(data) {
+        console.log(data);
+        // you would normally do a POST or PUT request here
+        // set submitted to true so that the Alert displays
+        setSubmitted(true);
+        // we need to provide a default values object to clear the React Select value
+        // https://react-hook-form.com/api/#reset
+        reset(DEFAULT_VALUES);
+    }
 
-	console.log(errors);
+    console.log(errors);
 
-	return (
-		<Container>
-			<h1>Registration</h1>
-			{/* if submitted is true display the Alert */}
-			{submitted && <Alert variant="success">Your registration was successful</Alert>}
-			<Form onSubmit={handleSubmit(onSubmit)}>
-				<Form.Group>
-					<Form.Control placeholder="Name" {...register("name")} />
-					<Form.Text className="text-muted">At least {MINIMUM_NAME_CHARACTERS} characters</Form.Text>
-					{errors.name && <ValidationError>{errors.name.message}</ValidationError>}
-				</Form.Group>
+    return (
+        <Container>
+            <h1>Registration</h1>
+            {/* if submitted is true display the Alert */}
+            {submitted && <Alert variant="success">Your registration was successful</Alert>}
+            <Form onSubmit={handleSubmit(onSubmit)}>
+                <Form.Group>
+                    <Form.Control placeholder="Name" {...register("name")} />
+                    <Form.Text className="text-muted">At least {MINIMUM_NAME_CHARACTERS} characters</Form.Text>
+                    {errors.name && <ValidationError>{errors.name.message}</ValidationError>}
+                </Form.Group>
 
-				<Form.Group>
-					<Form.Control placeholder="Email" {...register("email")} />
-					{errors.email && <ValidationError>{errors.email.message}</ValidationError>}
-				</Form.Group>
+                <Form.Group>
+                    <Form.Control placeholder="Email" {...register("email")} />
+                    {errors.email && <ValidationError>{errors.email.message}</ValidationError>}
+                </Form.Group>
 
-				<Form.Group>
-					<Form.Control type="password" placeholder="Password" {...register("password")} />
-					<Form.Text className="text-muted">At least 8 characters, with 1 number and 1 special character</Form.Text>
-					{errors.password && <ValidationError>{errors.password.message}</ValidationError>}
-				</Form.Group>
+                <Form.Group>
+                    <Form.Control type="password" placeholder="Password" {...register("password")} />
+                    <Form.Text className="text-muted">
+                        At least 8 characters, with 1 number and 1 special character
+                    </Form.Text>
+                    {errors.password && <ValidationError>{errors.password.message}</ValidationError>}
+                </Form.Group>
 
-				<Form.Group>
-					<Form.Control type="password" placeholder="Confirm password" {...register("confirmPassword")} />
-					{errors.confirmPassword && <ValidationError>{errors.confirmPassword.message}</ValidationError>}
-				</Form.Group>
+                <Form.Group>
+                    <Form.Control type="password" placeholder="Confirm password" {...register("confirmPassword")} />
+                    {errors.confirmPassword && <ValidationError>{errors.confirmPassword.message}</ValidationError>}
+                </Form.Group>
 
-				<Form.Group>
-					<Controller
+                <Form.Group>
+                    <Controller
                         name="skills"
                         control={control}
-                        render={({field}) => <Select isMulti options={SKILLS} {...field } />}
+                        render={({ field }) => <Select isMulti options={SKILLS} {...field} />}
                     />
-				</Form.Group>
+                </Form.Group>
 
-				<Button variant="info" type="submit">
-					Submit
-				</Button>
-			</Form>
-		</Container>
-	);
+                <Button variant="info" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+    );
 }
 
 export default App;
--- src/constants/registration.js
+++ src/constants/registration.js
@@ -1,18 +1,18 @@
 export const MINIMUM_NAME_CHARACTERS = 4;
 export const PASSPORT_REGEX = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
 export const SKILLS = [
-	{ value: "HTML", label: "HTML" },
-	{ value: "CSS", label: "CSS" },
-	{ value: "Sass", label: "Sass" },
-	{ value: "JavaScript", label: "JavaScript" },
-	{ value: "React", label: "React" },
-	{ value: "Angular", label: "Angular" },
-	{ value: "Vue", label: "Vue" },
-	{ value: "PHP", label: "PHP" },
-	{ value: "C#", label: "C#" },
-	{ value: "Wordpress", label: "Wordpress" },
-	{ value: "MySQL", label: "MySQL" },
+    { value: "HTML", label: "HTML" },
+    { value: "CSS", label: "CSS" },
+    { value: "Sass", label: "Sass" },
+    { value: "JavaScript", label: "JavaScript" },
+    { value: "React", label: "React" },
+    { value: "Angular", label: "Angular" },
+    { value: "Vue", label: "Vue" },
+    { value: "PHP", label: "PHP" },
+    { value: "C#", label: "C#" },
+    { value: "Wordpress", label: "Wordpress" },
+    { value: "MySQL", label: "MySQL" }
 ];
 export const DEFAULT_VALUES = {
-	skills: "",
+    skills: ""
 };
